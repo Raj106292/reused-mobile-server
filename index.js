1ef6c8fa4505a08c05d8a9053603219029209dab
Raj106292 <rajdas.106292@gmail.com>
2022-11-25 03:19:28 +0600

    provide jwt token for user

--- index.js
+++ index.js
@@ -13,23 +13,40 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.tkcvter.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async () => {
+    try {
+        const usersCollection = client.db('reused_project').collection('users');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+              $set: user,
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            console.log(result);
+
+            // generate jwt token
+            const token = jwt.sign(user, process.env.RMI_TOKEN, { expiresIn: '2d' })
+
+            res.send({ result, token });
+          });
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => {
+    console.log(error.message);
+})
+
+app.get('/', (req, res) => {
+    res.send('Mobile Island server is running');
+});
+
 app.listen(port, () => {
     console.log(`server is up and running on port ${port}`);
 });
-
-// app.put('/user/:email', async (req, res) => {
-//     const email = req.params.email;
-//     const user = req.body;
-//     const filter = { email: email };
-//     const options = { upsert: true };
-//     const updatedDoc = {
-//       $set: user,
-//     }
-//     const result = await usersCollection.updateOne(filter, updatedDoc, options);
-//     console.log(result);
-
-//     // generate jwt token
-//     const token = jwt.sign(user, process.env.ACCESS_TOKEN_AIRCNC, { expiresIn: '2d' })
-
-//     res.send({ result, token });
-//   });
